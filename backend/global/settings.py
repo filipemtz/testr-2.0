0d33023d97590cbf1ae65f36223a987878affa44
jcquadros <jullie.quadros@edu.ufes.br>
2024-07-01 02:28:54 -0300

    cleanup: Eliminando confgurações não utilizadas

--- backend/global/settings.py
+++ backend/global/settings.py
@@ -161,12 +161,6 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = config_data['backend']['cors_allowed_origins']
 CORS_ALLOW_CREDENTIALS = config_data['backend']['cors_allow_credentials']
 
-# CSRF_COOKIE_SECURE = config_data['backend']['csrf_cookie_secure']
-# SESSION_COOKIE_SECURE = config_data['backend']['session_cookie_secure']
-# CSRF_COOKIE_SAMESITE = config_data['backend']['csrf_cookie_samesite']
-# SESSION_COOKIE_SAMESITE = config_data['backend']['session_cookie_samesite']
-
-# CSRF_TRUSTED_ORIGINS = config_data['backend']['csrf_trusted_origins']
 
 
 
