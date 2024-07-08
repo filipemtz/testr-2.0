3da126750150ada1457fccb963f6a3781d748776
nathangarciaf <nathangarfreitas@gmail.com>
2024-07-08 01:51:32 -0300

    adjusts in io component

--- frontend/src/app/components/input-output/input-output.component.ts
+++ frontend/src/app/components/input-output/input-output.component.ts
@@ -6,6 +6,7 @@ import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Question } from '../../interfaces/question';
 import { QuestionService } from '../../services/question.service';
 import { InputOutput } from '../../interfaces/input-output';
+import { InputOutputService } from '../../services/input-output.service';
 
 @Component({
   selector: 'app-input-output',
@@ -19,23 +20,41 @@ export class InputOutputComponent {
   currentQuestion: Question = {} as Question;
   inputs_outputs: InputOutput[] = [];
 
+  defaultIO: InputOutput = {
+    id: -1,
+    url: '',
+    input: "",
+    output: "",
+    visible: true,
+    question: -1
+  }
+
   constructor (
     private route: ActivatedRoute,
     private questionService: QuestionService,
-
+    private ioService: InputOutputService
   ) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('questionId');
     if (id !== null){
       this.questionId = +id;
+      this.defaultIO.question = this.questionId;
       this.questionService.getInputsOutputs(this.questionId).subscribe({
-        next: ios => {
-          this.inputs_outputs = ios.results;
+        next: (ios : any) => {
+          this.inputs_outputs = ios;
         }
       })
     }
     else 
       console.error('Sem ID');
+  }
+
+  createInputOutput(): void {
+    this.ioService.postInputOutput(this.defaultIO).subscribe({
+      next: io => {
+        this.inputs_outputs.push(io);
+      }
+    })
   }
 }
