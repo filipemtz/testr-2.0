6ce872b736d39359c4c3c072cb7e52cae60bc5ae
nathangarciaf <nathangarfreitas@gmail.com>
2024-09-11 01:23:47 -0300

    table of students

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path('login/', UserLoginAPIView.as_view(), name='login'),
     path('logout/', UserLogoutAPIView.as_view(), name='logout'),
     path('register/', UserRegisterAPIView.as_view(), name='register'),
-    path('register-student-csv/', StudentCSVRegisterAPIView.as_view(), name='register-student'),
     path('profile/', UserProfileAPIView.as_view(), name='profile'),
     # path('refresh/', UserRefreshAPIView.as_view(), name='refresh'),
     path('change-password/', ChagePasswordAPIView.as_view(), name='change-password'),
--- backend/accounts/views/user_register.py
+++ backend/accounts/views/user_register.py
@@ -28,36 +28,3 @@ class UserRegisterAPIView(APIView):
             'user': UserSerializer(user).data
         })
     
-class StudentCSVRegisterAPIView(APIView):
-    def post(self, request):
-        csv_file = request.FILES['file']
-        decoded_file = csv_file.read().decode('utf-8').splitlines()
-        reader = csv.DictReader(decoded_file, delimiter=';')
-        
-        responses = []
-        
-        for row in reader:
-            data = {
-                'username': row['username'],
-                'password': row['password'],
-                'password_confirm': row['password'], 
-                'email': row['email'],
-                'first_name': row['first_name'],
-                'last_name': row['last_name'],
-                'group': 'student'
-            }
-            
-            serializer = UserRegisterSerializer(data=data)
-            
-            if serializer.is_valid():
-                serializer.save()
-                token, created = Token.objects.get_or_create(user=serializer.instance)
-                user = User.objects.filter(username=serializer.instance.username).first()
-                responses.append({
-                    'token': token.key, 
-                    'user': UserSerializer(user).data
-                })
-            else:
-                responses.append({'error': serializer.errors, 'data': data})
-        
-        return Response(responses)
